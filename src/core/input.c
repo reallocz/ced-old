25964162e40e01a57706f80126e8d09acd18029e
realloc <realloc@protonmail.com>
2018-02-15 13:31:58 +0530

    Add more keys to _inp_gettype, and GKEY macro

--- src/core/input.c
+++ src/core/input.c
@@ -3,15 +3,49 @@
 #include <stdio.h>
 #include <assert.h>
 
+// macros
 
-/** Get the type of command from the key */
+/** Add GLFW_KEY_ prefix */
+#define GKEY(K) GLFW_KEY_##K
+/** Compare key with GKEY. *NOTE*: 'key' must be in scope! */
+#define KEYIS(K) (key == GKEY(K))
+
+/** Get the type of command from the key
+ * \see http://www.glfw.org/docs/latest/group__keys.html
+ */
 enum doccmd_type _inp_gettype(inpkey ik)
 {
 	int key = ik.key; // alias
 
-	if (key >= GLFW_KEY_A && key <= GLFW_KEY_Z) {
+	// All alpha-numeric keys and some symbols
+	// TODO add rest of the symbols
+	if ((key >= 65 && key <= 90)			// Alpha keys
+			|| (key >= 48 && key <= 57)		// Numeric (numpad) keys
+			|| (key >= 320 && key <= 329)	// Numeric (keypad) keys
+			|| (key >= 44 && key <= 47)		// Some symbols
+			|| (key >= 91 && key <= 98)	// [, /, ]
+			|| KEYIS(SPACE) || KEYIS(APOSTROPHE)	|| KEYIS(SEMICOLON)
+			|| KEYIS(EQUAL) || KEYIS(TAB)
+			)
+	{
 		return INSERT;
-	} else {
+	}
+
+	else if(KEYIS(BACKSPACE) || KEYIS(DELETE))
+	{
+		return EDIT;
+	}
+
+	else if(KEYIS(UP) || KEYIS(DOWN) || KEYIS(LEFT) || KEYIS(RIGHT)
+				|| KEYIS(PAGE_UP) || KEYIS(PAGE_DOWN) || KEYIS(HOME)
+				|| KEYIS(END)
+			)
+	{
+		return MOVE;
+	}
+
+	else
+	{
 		return NOP;
 	}
 }
@@ -19,11 +53,12 @@ enum doccmd_type _inp_gettype(inpkey ik)
 
 void inp_onkey(window* win, inpkey ik)
 {
-	/* ignore key releases for now */
 	if(!win_getdoc(win)) {
 		printf("No document set!\n");
 		return;
 	}
+
+	/* ignore key releases for now */
 	if(ik.action == GLFW_RELEASE)
 		return;
 
@@ -32,16 +67,17 @@ void inp_onkey(window* win, inpkey ik)
 		return;
 	}
 
-	enum doccmd_type type = _inp_gettype(ik);
 	document* doc = win_getdoc(win);
-	doccmd cmd = dc_create(type);
+	doccmd cmd = dc_create(_inp_gettype(ik));
 
 	/* Dispatch to doc_exec */
-	switch (type) {
+	switch (cmd.type) {
 		case (INSERT):
 			cmd.data.c = ik.key;
 			doc_exec(doc, cmd);
 			break;
+		case (EDIT):
+			doc_exec(doc, cmd);
 		default:
 			break;
 	}
