e4e779bd8db6d39b9560f2d48e3e123a0bf3ebb1
realloc <realloc@protonmail.com>
2018-02-17 20:39:05 +0530

    Add vec2, vec3, and vec4 structs

--- include/mymaths.h
+++ include/mymaths.h
@@ -5,3 +5,21 @@
 #define MIN(A, B) (A < B ? A : B)
 #define MAX(A, B) (A > B ? A : B)
 
+typedef struct vec2 {
+	int x;
+	int y;
+} vec2;
+
+typedef struct vec3 {
+	int x;
+	int y;
+	int z;
+} vec3;
+
+typedef struct vec4 {
+	int x;
+	int y;
+	int z;
+	int w;
+} vec4;
+
